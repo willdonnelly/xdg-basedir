3dc20e62ac01702795c5a681538ce2a63b00fc4a
Will Donnelly <will.donnelly@gmail.com>
2009-07-20 21:36:44 -0500

    Reduced quantity of non-portable code.

--- Data/XDG/BaseDir.hs
+++ Data/XDG/BaseDir.hs
@@ -14,66 +14,68 @@ import Data.Maybe         ( fromMaybe )
 import System.FilePath    ( (</>) )
 import System.Environment ( getEnvironment )
 import System.IO.Error    ( try )
+import Text.Regex         ( splitRegex, mkRegex, Regex )
+import System.Directory   ( getHomeDirectory )
 
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
 
+defaultDir "XDG_DATA_HOME"   = userRelative $ ".local" </> "share"
+defaultDir "XDG_CONFIG_HOME" = userRelative $ ".config"
+defaultDir "XDG_CACHE_HOME"  = userRelative $ ".cache"
+defaultDir "XDG_DATA_DIRS"   = return $ "/usr/local/share:/usr/share"
+defaultDir "XDG_CONFIG_DIRS" = return $ "/etc/xdg"
+defaultDir _                 = return $ ""
+
 #else
 
-import System.Directory   ( getHomeDirectory )
-import Text.Regex         ( splitRegex, mkRegex, Regex )
+defaultDir "XDG_DATA_HOME"   = userRelative $ ".local" </> "share"
+defaultDir "XDG_CONFIG_HOME" = userRelative $ ".config"
+defaultDir "XDG_CACHE_HOME"  = userRelative $ ".cache"
+defaultDir "XDG_DATA_DIRS"   = return $ "/usr/local/share:/usr/share"
+defaultDir "XDG_CONFIG_DIRS" = return $ "/etc/xdg"
+defaultDir _                 = return $ ""
 
 #endif
 
 getUserDataDir      :: String -> IO FilePath
+getUserDataDir      = singleDir "XDG_DATA_HOME"
+
 getUserConfigDir    :: String -> IO FilePath
+getUserConfigDir    = singleDir "XDG_CONFIG_HOME"
+
 getUserCacheDir     :: String -> IO FilePath
+getUserCacheDir     = singleDir "XDG_CACHE_HOME"
+
 getSystemDataDirs   :: String -> IO [FilePath]
+getSystemDataDirs   = multiDirs "XDG_DATA_DIRS"
+
 getSystemConfigDirs :: String -> IO [FilePath]
+getSystemConfigDirs = multiDirs "XDG_CONFIG_DIRS"
+
 getAllDataDirs      :: String -> IO [FilePath]
-getAllConfigDirs    :: String -> IO [FilePath]
-
-#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
-
-getUserDataDir app = undefined
-getUserConfigDir app = undefined
-getUserCacheDir app = undefined
-getSystemDataDirs app = undefined
-getSystemConfigDirs app = undefined
-
-#else
-
-getUserDataDir app = getXdgDir app (".local" </> "share") "XDG_DATA_HOME"
-getUserConfigDir app = getXdgDir app ".config" "XDG_CONFIG_HOME"
-getUserCacheDir app = getXdgDir app ".cache" "XDG_CACHE_HOME"
-
-getSystemDataDirs app = do
-    dataDirs <- envLookup "XDG_DATA_DIRS"
-    let defaultDirs = "/usr/local/share:/usr/share"
-    let dirsList = splitRegex (mkRegex ":") $ fromMaybe defaultDirs dataDirs
-    return . map (</> app) $ dirsList
-
-getSystemConfigDirs app = do
-    dataDirs <- envLookup "XDG_CONFIG_DIRS"
-    let defaultDirs = "/etc/xdg"
-    let dirsList = splitRegex (mkRegex ":") $ fromMaybe defaultDirs dataDirs
-    return . map (</> app) $ dirsList
-
-getXdgDir :: String -> FilePath -> String -> IO FilePath
-getXdgDir app defPath envEntry = do
-    homeDir <- getHomeDirectory
-    let defaultDir = homeDir </> defPath
-    xdgDir <- envLookup envEntry
-    return $ fromMaybe defaultDir xdgDir </> app
-
-#endif
-
 getAllDataDirs app = do userDir <- getUserDataDir app
                         sysDirs <- getSystemDataDirs app
                         return $ userDir:sysDirs
 
+getAllConfigDirs    :: String -> IO [FilePath]
 getAllConfigDirs app = do userDir <- getUserConfigDir app
                           sysDirs <- getSystemConfigDirs app
                           return $ userDir:sysDirs
 
-envLookup :: String -> IO (Maybe String)
-envLookup key = getEnvironment >>= (return . lookup key)
+singleDir :: String -> String -> IO FilePath
+singleDir key app = envLookup key >>= return . (</> app)
+
+multiDirs :: String -> String -> IO [FilePath]
+multiDirs key app = envLookup key >>= return . map (</> app) . splitDirs
+
+splitDirs :: String -> [String]
+splitDirs = splitRegex $ mkRegex ":"
+
+userRelative :: FilePath -> IO FilePath
+userRelative p = getHomeDirectory >>= return . (</> p)
+
+envLookup :: String -> IO String
+envLookup key = do env <- getEnvironment
+                   case lookup key of
+                        Just val = val
+                        Nothing  = getDefault key
