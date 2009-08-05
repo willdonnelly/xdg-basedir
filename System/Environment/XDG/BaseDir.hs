6fb24458054ac8dfa3b460fe14a5e0c9e14cb1ee
Will Donnelly <will.donnelly@gmail.com>
2009-08-05 17:21:00 -0500

    Added 'getXFiles' functions.

--- System/Environment/XDG/BaseDir.hs
+++ System/Environment/XDG/BaseDir.hs
@@ -8,9 +8,13 @@ module System.Environment.XDG.BaseDir
     , getUserCacheDir
     , getUserCacheFile
     , getSystemDataDirs
+    , getSystemDataFiles
     , getSystemConfigDirs
+    , getSystemConfigFiles
     , getAllDataDirs
+    , getAllDataFiles
     , getAllConfigDirs
+    , getAllConfigFiles
     ) where
 
 import Data.Maybe         ( fromMaybe )
@@ -19,6 +23,7 @@ import System.Environment ( getEnvironment, getEnv )
 import System.IO.Error    ( try )
 import Text.Regex         ( splitRegex, mkRegex, Regex )
 import System.Directory   ( getHomeDirectory )
+import Control.Monad      ( liftM2 )
 
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
 
@@ -51,10 +56,16 @@ getUserConfigDir    = singleDir "XDG_CONFIG_HOME"
 getUserCacheDir     = singleDir "XDG_CACHE_HOME"
 getSystemDataDirs   = multiDirs "XDG_DATA_DIRS"
 getSystemConfigDirs = multiDirs "XDG_CONFIG_DIRS"
+getAllDataDirs a    = liftM2 (:) (getUserDataDir a) (getSystemDataDirs a)
+getAllConfigDirs a  = liftM2 (:) (getUserConfigDir a) (getSystemConfigDirs a)
 
-getUserDataFile a f   = fmap (</> f) $ getUserDataDir a
-getUserConfigFile a f = fmap (</> f) $ getUserConfigDir a
-getUserCacheFile a f  = fmap (</> f) $ getUserCacheDir a
+getUserDataFile a f      = fmap (</> f) $ getUserDataDir a
+getUserConfigFile a f    = fmap (</> f) $ getUserConfigDir a
+getUserCacheFile a f     = fmap (</> f) $ getUserCacheDir a
+getSystemDataFiles a f   = fmap (map (</> f)) $ getSystemDataDirs a
+getSystemConfigFiles a f = fmap (map (</> f)) $ getSystemConfigDirs a
+getAllDataFiles a f      = fmap (map (</> f)) $ getAllDataDirs a
+getAllConfigFiles a f    = fmap (map (</> f)) $ getAllConfigDirs a
 
 singleDir :: String -> String -> IO FilePath
 singleDir key app = envLookup key >>= return . (</> app)
@@ -67,15 +78,6 @@ envLookup key = do env <- getEnvironment
                    case lookup key env of
                         Just val -> return val
                         Nothing  -> getDefault key
-getAllDataDirs      :: String -> IO [FilePath]
-getAllDataDirs app = do userDir <- getUserDataDir app
-                        sysDirs <- getSystemDataDirs app
-                        return $ userDir:sysDirs
-
-getAllConfigDirs    :: String -> IO [FilePath]
-getAllConfigDirs app = do userDir <- getUserConfigDir app
-                          sysDirs <- getSystemConfigDirs app
-                          return $ userDir:sysDirs
 
 userRelative :: FilePath -> IO FilePath
 userRelative p = getHomeDirectory >>= return . (</> p)
